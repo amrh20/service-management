5545cd7a0903a93761efb795245df18df9a182f8
amrohamdy <amrhamdyragab@gmail.com>
2025-01-24 20:30:10 +0200

    complete responsive

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { BreadcrumbComponent } from './../breadcrumb/breadcrumb.component';
 import { RouterModule } from '@angular/router';
 import { SideMenuComponent } from './../side-menu/side-menu.component';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [SideMenuComponent, RouterModule, BreadcrumbComponent, CommonModule],
+  imports: [SideMenuComponent, RouterModule, BreadcrumbComponent, CommonModule, TableComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   standalone: true,
@@ -30,6 +31,41 @@ export class LayoutComponent {
     this.sideMenuStatus = !this.sideMenuStatus
     document.body.classList.remove('overflow-hidden');
 
+  }
+
+  @ViewChild(TableComponent) table!: TableComponent;
+
+
+  serviceData: any[] = [];
+  tableColumns: any[] = [
+    { field: 'serviceName', header: 'Service Name' },
+    { field: 'code', header: 'Code' },
+    { field: 'type', header: 'Type' },
+    { field: 'status', header: 'Status' }
+  ];
+
+  ngOnInit(): void {
+    // Adding dummy data to serviceData
+    this.serviceData = [
+      { serviceName: 'Property Transfer', code: 'P-T01', type: 'Service Type', status: 'Active' },
+      { serviceName: 'Property Ownership', code: 'P-O2', type: 'Service Type', status: 'Active' },
+      { serviceName: 'Property Transfer', code: 'P-T01', type: 'Service Type', status: 'Inactive' },
+      { serviceName: 'Property Ownership', code: 'P-O2', type: 'Service Type', status: 'Inactive' },
+      { serviceName: 'Property Transfer', code: 'P-T01', type: 'Service Type', status: 'Active' },
+      { serviceName: 'Property Ownership', code: 'P-O2', type: 'Service Type', status: 'Active' },
+      { serviceName: 'Property Transfer', code: 'P-T01', type: 'Service Type', status: 'Inactive' },
+      { serviceName: 'Property Ownership', code: 'P-O2', type: 'Service Type', status: 'Inactive' },
+      { serviceName: 'Property Transfer', code: 'P-T01', type: 'Service Type', status: 'Active' },
+      { serviceName: 'Property Ownership', code: 'P-O2', type: 'Service Type', status: 'Active' },
+      { serviceName: 'Property Transfer', code: 'P-T01', type: 'Service Type', status: 'Inactive' },
+      { serviceName: 'Property Ownership', code: 'P-O2', type: 'Service Type', status: 'Inactive' }
+    ];
+  }
+
+
+  onSearch(filters: any): void {
+    // Pass the filters to the table component
+    this.table.onSearch(filters);
   }
 
 }
